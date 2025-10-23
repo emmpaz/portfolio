9aaf466560a957fe8e04948756ae15bdfaf7c3fe
Emmanuel Paz <emmanuel.paz@oracle.com>
2025-10-23 13:13:50 -0600

    adding new experience and refractoring redundant code

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -11,65 +11,103 @@ const container = {
     },
 };
 
+type Experience = {
+    title: string,
+    timeline: string,
+    bullets: string[],
+    location: string,
+    company: string,
+    inView: boolean
+}
+
 function Experience({ reference, inView }: { reference: any; inView: boolean }) {
+
+    const experiences: Experience[] = [
+        {
+            title: "Production Software Engineer",
+            timeline: "Oct. 2025 - Present",
+            bullets:  [
+                    "Loading..."
+                ]
+            ,
+            location: "Salt Lake City, UT",
+            company: "Oracle",
+            inView: inView
+        },
+        {
+            title: "Associate Developer Support Engineer",
+            timeline: "Aug. 2024 - Oct. 2025",
+            bullets:  [
+                    "Loading.."
+                ]
+            ,
+            location: "Salt Lake City, UT",
+            company: "Okta",
+            inView: inView
+        },
+        {
+            title: "Prompt Engineer",
+            timeline: "Dec. 2023 - Aug. 2024",
+            bullets: [
+                    "Developed coding problems and solutions for training multiple LLM coding chatbots for performing data analytics",
+                    "Evaluated code quality produced by AI models for correctness, performance, and helpfulness to the user",
+                    "Train models with languages including Python, Javascript, C++, NodeJS, ReactJS, and ExpressJS",
+                    "Participated in code reviews, making sure submissions were correct and followed style guidelines"
+                ]
+            ,
+            location: "Chicago, IL",
+            company: "Data Annotation",
+            inView: inView
+        },
+        {
+            title: "Web Developer",
+            timeline: "Sep. 2022 - Feb. 2023",
+            bullets: [
+                    "Initiated and pitched the concept for a user-friendly website using React.js, securing a contract valued at $800",
+                    "Improved website navigation with React Routes, making it easy for users to explore, resulting in positive feedback",
+                    "Leveraging CSS/HTML to website look great on all devices by using CSS for responsiveness"
+                ]
+            ,
+            location: "United States",
+            company: "Tom Concrete LLC",
+            inView: inView
+        },
+        {
+            title: "Software Engineering Intern",
+            timeline: "June 2022 - August 2022",
+            bullets: [
+                    "Collaborated a team to build a web app for a retail services client that needed a centralized dashboard",
+                    "Served as lead backend engineer to create all API endpoints powered by Spring Boot, SQL and Azure Cloud",
+                    "Contributed to frontend focusing on the REST API calls (CRUD) using Axios to ensure consistency in data",
+                    "Appeared to daily scrum meetings about UI/Architecture design, feature discovery, and application development"
+                ]
+            ,
+            location: "Chicago, IL",
+            company: "Publicis Sapient",
+            inView: inView
+        }
+    ]
+
 
     return (
         <motion.div className={`min-h-screen w-full grid section opacity-0 transform transition duration-2000 ease-out ${inView ? 'opacity-100 translate-y-0' : ''
             }}`} ref={reference}>
             <div className="w-full h-full font-incon text-slate-50 flex justify-center items-center flex-col sm:flex-row ">
-                <motion.div 
+                <motion.div
                     variants={container}
                     animate={inView ? 'visible' : 'hidden'}
                     className="w-1/2 max-w-xl min-w-80 flex flex-col items-center">
-                    <Card
-                        title="Associate Developer Support Engineer"
-                        timeline="Aug. 2024 - Present"
-                        bullets = {[
-                            "Loading.."
-                        ]}
-                        
-                        location="Salt Lake City, UT"
-                        company="Okta"
-                        inView={inView}
-                    />
-                    <Card
-                        title="Prompt Engineer"
-                        timeline="Dec. 2023 - Aug. 2024"
-                        bullets={[
-                            "Developed coding problems and solutions for training multiple LLM coding chatbots for performing data analytics",
-                            "Evaluated code quality produced by AI models for correctness, performance, and helpfulness to the user",
-                            "Train models with languages including Python, Javascript, C++, NodeJS, ReactJS, and ExpressJS",
-                            "Participated in code reviews, making sure submissions were correct and followed style guidelines"
-                        ]}
-                        location="Chicago, IL"
-                        company="Data Annotation"
-                        inView={inView}
-                    />
-                    <Card
-                        title="Web Developer"
-                        timeline="Sep. 2022 - Feb. 2023"
-                        bullets={[
-                            "Initiated and pitched the concept for a user-friendly website using React.js, securing a contract valued at $800",
-                            "Improved website navigation with React Routes, making it easy for users to explore, resulting in positive feedback",
-                            "Leveraging CSS/HTML to website look great on all devices by using CSS for responsiveness"
-                        ]}
-                        location="United States"
-                        company="Tom Concrete LLC"
-                        inView={inView}
-                    />
-                    <Card
-                        title="Software Engineering Intern"
-                        timeline="June 2022 - August 2022"
-                        bullets={[
-                            "Collaborated a team to build a web app for a retail services client that needed a centralized dashboard",
-                            "Served as lead backend engineer to create all API endpoints powered by Spring Boot, SQL and Azure Cloud",
-                            "Contributed to frontend focusing on the REST API calls (CRUD) using Axios to ensure consistency in data",
-                            "Appeared to daily scrum meetings about UI/Architecture design, feature discovery, and application development"
-                        ]}
-                        location="Chicago, IL"
-                        company="Publicis Sapient"
-                        inView={inView}
-                    />
+                    {
+                        experiences.map((entry) => (
+                        <Card
+                            title={entry.title}
+                            timeline={entry.timeline}
+                            bullets={entry.bullets}
+                            location={entry.location}
+                            company={entry.company}
+                            inView={entry.inView}
+                        />))
+                    }
                 </motion.div>
                 <div className="mt-10 sm:mt-0">
                     <p>see <a href={resume} target="_blank" className=" underline">resume</a> for details</p>
